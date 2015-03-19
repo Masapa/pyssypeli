8f6349e8026dd4e9c0522559eb675f8a0f82a325
Timo Peltoniemi <timmy.peltoniemi@gmail.com>
2015-03-19 12:21:36 +0200

    testiä

--- Pyssypeli/Assets/scripts/human.cs
+++ Pyssypeli/Assets/scripts/human.cs
@@ -5,7 +5,7 @@ public class human : MonoBehaviour {
 	// asdfasdfasdffasd kommenttia
 	// Use this for initialization
 	void Start () {
-	
+	// usetknasdnoijasdfåoijasdfoijå
 	}
 	
 	// Update is called once per frame
