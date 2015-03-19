9eb59e9195e771022c21edf6736c8c7c569fa8de
masapa <matti@passoja.fi>
2015-03-19 09:42:11 +0200

    testttt

---
+++ Pyssypeli/Assets/scripts/testi.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int c = 23;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
