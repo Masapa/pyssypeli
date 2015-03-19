44b9d244ce103f0fd5f79fc29f6c0e8e7ef1d9b6
masapa <matti@passoja.fi>
2015-03-19 16:27:47 +0200

    Paljon shaissea. Luovutan tän näin tekemällä ja teen valmiin "pohjan" mihi randomilla teen huoneet jne.

--- Pyssypeli/Assets/scripts/Mappigene.cs
+++ Pyssypeli/Assets/scripts/Mappigene.cs
@@ -1,21 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class Mappigene : MonoBehaviour {
-	public int x = 2;
+	public int x = 3;
 	public int y = 3;
-	Rigidbody2D testi = new Rigidbody2D ();
-	
-	SpriteRenderer testi1 = new SpriteRenderer ();
 
-	public Sprite testii;
+	List<List<GameObject>> testi = new List<List<GameObject>>();
+	public GameObject testii2;
+	//float pituus = ;
 	// Use this for initialization
+	int a;
+	float nextx;
+	float pituus;
+	float nexty;
 	void Start () {
-		testi1.sprite = testii;
-		testi1.enabled = true;
+		pituus = testii2.transform.gameObject.GetComponent<SpriteRenderer> ().bounds.extents.x;
+		Debug.Log ("Vittu se on" + pituus);
+		nexty = 0;
+		for (int i = 0; i<y; i++) {
+			testi.Add (new List<GameObject>());
+			nextx = 0;
+			for(a = 0;a<x;a++){
+				testi[i].Add ((GameObject)Instantiate(testii2));
+				if(i+1%3==1&&a%2 == 0){nextx = pituus;testi[i][a].transform.position = new Vector2(nextx,nexty); testi[i][a].transform.Rotate(0,0,30);nextx +=pituus*1.5f;}
+				if(i+1%3==1&&a%2 == 1){testi[i][a].transform.position = new Vector2(nextx,nexty); testi[i][a].transform.Rotate(0,0,180-30);nextx += pituus*2;}
 
-		testi1.transform.localPosition = new Vector2 (0, 0);
+				if(i+1%3==2){testi[i][a].transform.position = new Vector2(nextx,nexty); testi[i][a].transform.Rotate(0,0,90);nextx +=pituus*4;}
 
+
+				if(i+1%3==0&&a%2 == 0){testi[i][a].transform.position = new Vector2(nextx,nexty); testi[i][a].transform.Rotate(0,0,30);nextx +=testi[i][a].transform.gameObject.GetComponent<SpriteRenderer> ().bounds.size.x-0.5f;}
+				if(i+1%3==0&&a%2 == 1){testi[i][a].transform.position = new Vector2(nextx,nexty); testi[i][a].transform.Rotate(0,0,180-30);nextx += testi[i][a].transform.gameObject.GetComponent<SpriteRenderer> ().bounds.size.x;}
+
+				
+			}
+
+
+			nexty -= pituus*1.5f;
+		}
+
+	/*	testi.Add ((GameObject)Instantiate(testii2));
+		//seina other = testi [0].GetComponent ("seina");
+		//other.tila (-2, 0);
+		testi [0].transform.position = new Vector2 (-2, 0);
+		testi [1].transform.position = new Vector2 (4, 3);
+		testi[0].transform.Rotate (new Vector3(0,0,30));
+
+		seina seeeina = (seina)testi [0].GetComponent ("Seina");
+
+		//seeeina.transform.position = new Vector2 (-2, 3);
+
+		//Instantiate (seina);*/
 	}
 	
 	// Update is called once per frame
--- Pyssypeli/Assets/scripts/seina.cs
+++ Pyssypeli/Assets/scripts/seina.cs
@@ -5,24 +5,38 @@ public class seina : MonoBehaviour {
 	public bool kulma = false; // onko kulmassa vai suorassa;
 	public bool oikea = false; // kulma oikeaan vai vasempaan;
 	public bool ala = false;
+	public float x;
+	public float y;
+	Vector3 testiii;
 	// Use this for initialization
 	void Start () {
-		if (!ala && kulma && !oikea)
-			transform.Rotate (0, 0, 60);
-		if (oikea && kulma && !ala)
-			transform.Rotate (0, 0, 60 + 60 + 60 + 60 + 60);
-		if (ala && oikea && kulma)
-			transform.Rotate (0, 0, 60 + 60);
-		if (ala && !oikea && kulma)
-			transform.Rotate (0, 0, 60 + 60 + 60 + 60);
+	/*	if (ala && kulma && !oikea)
+			transform.Rotate (0, 0, -30);
+		if (ala&&oikea && kulma )
+			transform.Rotate (0, 0, 30+180);
+		if (!ala && oikea && kulma)
+			transform.Rotate (0, 0, 180-30);
+		if (!ala && !oikea && kulma)
+			transform.Rotate (0, 0, 30);
 		if (!kulma) {
-			transform.Rotate (0, 0, 0);
-		}
+			transform.Rotate (0, 0, 90);
+		}*/
+		//tila (2, 4);
+
+		//transform.localScale = new Vector2 (2, 1);
+	
+		float testiii = transform.gameObject.GetComponent<SpriteRenderer> ().bounds.extents.x;
+		Debug.Log (testiii);
+	}
+	public void tila(float x,float y){
+		this.transform.position = new Vector3 (x, y,0);
+
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		x = transform.position.x;
+		y = transform.position.y;
 	}
 }
