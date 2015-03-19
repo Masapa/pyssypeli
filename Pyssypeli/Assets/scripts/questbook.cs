05cd66a1c7287e440f4f6b4df0a832cbe6e2e219
Timo Peltoniemi <timmy.peltoniemi@gmail.com>
2015-03-19 14:03:42 +0200

    Muokattu Ollin kans c# objekteja unityä varten

---
+++ Pyssypeli/Assets/scripts/questbook.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class human : MonoBehaviour 
+
+{
+
+	private class questbook
+	{
+		private string questName;
+	}
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+
+	public void addExp()
+	{
+	
+	}
+	public void addMoney()
+	{
+	
+	}
+	public void openQuestMenu()
+	{
+	
+	}
+	public void closeQuestMenu()
+	{
+	
+	}
+	public void addQuest()
+	{
+	
+	}
+	public void removeQuest()
+	{
+	
+	}
+}
