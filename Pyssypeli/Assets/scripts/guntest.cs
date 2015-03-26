35a08f078054e74079f3429334b087b0a6bcf02f
Tony Asplund <tony.asplund@cou.fi>
2015-03-26 14:08:34 +0200

    pyssyjä

---
+++ Pyssypeli/Assets/scripts/guntest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class guntest : MonoBehaviour {
+
+
+
+	public float fireRate = 0;
+	public float damage = 10;
+	public LayerMask notToHit;
+
+	float timeToFire = 0;
+	Transform firePoint;
+
+
+	void Awake(){
+		firePoint = transform.FindChild ("FirePoint");
+		if (firePoint == null) {
+			Debug.LogError("no firepointtia whattaa fuuuu");
+		}
+		
+	}
+
+	void Update(){
+		if (fireRate == 0) {
+			if (Input.GetButtonDown ("Fire1")) {
+				Shoot ();
+			}
+		} else {
+			if (Input.GetButton ("Fire1") && Time.time > timeToFire){
+				timeToFire = Time.time + 1/fireRate;
+				Shoot();
+			}
+		}
+	}
+
+	void Shoot(){
+		Debug.Log ("tää toimii pikkukoira");
+
+	}
+
+}
