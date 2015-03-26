f3a01741cb6695e7e879acae7eaff39a592bad84
Timo Peltoniemi <timmy.peltoniemi@gmail.com>
2015-03-26 09:14:31 +0200

    Korjattu Ollin kans periytykset luokista

---
+++ Pyssypeli/Assets/scripts/hero.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hero : human 
+
+{
+
+	private int attributePointPool ;
+	private int exp ;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+	
+	public void openInventory()
+	{
+		
+	}
+	public void closeInventory()
+	{
+		
+	}
+	public void pickupItem()
+	{
+		
+	}
+	public void equipItem()
+	{
+		
+	}
+	public void unequipItem()
+	{
+		
+	}
+	public void useItem()
+	{
+		
+	}
+
+}
---
+++ Pyssypeli/Assets/scripts/npc.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class npc : human 
+
+{
+
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	public void levelCharacterUp()
+	{
+	
+	}
+	public void levelWeaponUp()
+	{
+	
+	}
+	public void openTradeMenu()
+	{
+	
+	}
+	public void closeTradeMenu()
+	{
+	
+	}
+	public void addAgility()
+	{
+	
+	}
+	public void addConstitution()
+	{
+	
+	}
+	public void addStrength()
+	{
+	
+	}
+	public void addLuck()
+	{
+	
+	}
+
+}
