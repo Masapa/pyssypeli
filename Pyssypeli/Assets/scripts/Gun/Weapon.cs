60f9a604dace2055c49edec5119f041c129d05f7
Kolppan3n <kolppan3n@gmail.com>
2015-04-02 18:55:34 +0300

    nyt pyssy toimii, kansiokkin ois hyvä laittaa englanniksi niin sama tyyli kaikkialla -_-

--- Pyssypeli/Assets/scripts/Gun/Weapon.cs
+++ Pyssypeli/Assets/scripts/Gun/Weapon.cs
@@ -3,12 +3,9 @@ using System.Collections;
 
 public class Weapon : MonoBehaviour {
 	
-	
-	
 	public float fireRate = 0;
 	public float Damage = 10;
-	//public LayerMask whatToHit;
-	public GameObject MagicBullet;
+	public GameObject Bullet;
 	
 	float timeToFire = 0;
 	Transform firePoint;
@@ -23,21 +20,30 @@ public class Weapon : MonoBehaviour {
 	}
 	
 	void Update(){
-		if (fireRate == 0) {
-			if (Input.GetButtonDown ("Fire1")) {
+		if (fireRate == 0) 
+		{
+			if (Input.GetButtonDown ("Fire1")) 
+			{
 				Shoot ();
 			}
-		} else {
-			if (Input.GetButton ("Fire1") && Time.time > timeToFire){
+		}
+
+		else 
+		{
+			if (Input.GetButton ("Fire1") && Time.time > timeToFire)
+			{
 				timeToFire = Time.time + 1/fireRate;
 				Shoot();
 			}
 		}
 	}
-	//4:25
+
+
 	void Shoot()
 	{
-		Instantiate (MagicBullet, transform.position, Quaternion.identity);
+		Vector3 pos = transform.position;
+
+		Instantiate (Bullet, transform.position, transform.rotation);
 
 	}
 		
