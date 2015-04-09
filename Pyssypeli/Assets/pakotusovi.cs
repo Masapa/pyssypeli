d257f11436725c52eb12e2424b45330f25ff68c8
Olli Isohanni <ollih2@gmail.com>
2015-04-09 13:21:33 +0300

    Toimiva seinaovi med lukko

---
+++ Pyssypeli/Assets/pakotusovi.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pakotusovi : MonoBehaviour {
+	Rigidbody2D rb;
+	public bool vittuovi = false;
+	Quaternion alkutila;
+	// Use this for initialization
+	void Start () {
+		 rb = GetComponent<Rigidbody2D> ();
+		alkutila = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!vittuovi) {
+			transform.rotation = rb.transform.rotation;
+			//rb.isKinematic = false;
+			rb.fixedAngle = false;
+		} else{
+			//rb.isKinematic = true;
+			rb.fixedAngle = true;
+			transform.rotation = alkutila;
+		rb.transform.rotation = alkutila;
+	}
+	}
+}
