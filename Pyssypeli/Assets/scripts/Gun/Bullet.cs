baf868f85d457d7d5b3a4e785ff0694e47aff315
masapa <matti@passoja.fi>
2015-04-09 10:04:56 +0300

    Bullettia korjattu

--- Pyssypeli/Assets/scripts/Gun/Bullet.cs
+++ Pyssypeli/Assets/scripts/Gun/Bullet.cs
@@ -4,16 +4,24 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
 
 	public float Speed = 8f;
+	Rigidbody2D rb;
+	void Start(){
+		rb = GetComponent<Rigidbody2D> ();
+
+
+	}
 
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
-		Vector3 pos = transform.position;
+		Vector2 pos = transform.position;
 
-		Vector3 velocity = new Vector3 (0, Speed * Time.deltaTime, 0);
+		//Vector2 velocity = new Vector3 (0, Speed * Time.deltaTime);
+		rb.velocity = rb.transform.rotation * Vector2.up * Speed;
 
-		pos += transform.rotation * velocity;
 
-		transform.position = pos;
+		//pos += transform.rotation * velocity;
+
+		//transform.position = pos;
 	}
 }
